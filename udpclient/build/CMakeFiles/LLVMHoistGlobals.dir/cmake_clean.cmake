df30a168919a143147f32e32aa60ba24a344533e
matteomartelli <matteomartelli3@gmail.com>
2014-06-16 12:35:42 +0200

    Gitignore now works. Deleting build directories from the branch.

--- udpclient/build/CMakeFiles/LLVMHoistGlobals.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/LLVMHoistGlobals"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/LLVMHoistGlobals.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
