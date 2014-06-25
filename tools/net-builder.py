2d75249a592b7cd08b213fc26865a2396275cd32
berdav <berardi.dav@gmail.com>
2014-06-25 19:20:28 +0200

    Working example and: + Geolocalized random topology + Correct Scallion/TOR command line arguments

--- tools/net-builder.py
+++ tools/net-builder.py
@@ -22,7 +22,7 @@
 # USA.                                                                        #
 ###############################################################################
 
-# TODO : topology and tracking
+# TODO : truly random topology, tracking, user interface and code-polishing
 
 import xml.etree.ElementTree as ET;
 import xml.dom.minidom as MD;
@@ -31,18 +31,48 @@ import random;
 DEFAULT_TOR_ARGS = "1024 --quiet --Address ${NODEID} --Nickname ${NODEID} --DataDirectory ./data/${NODEID} --GeoIPFile ~/.shadow/share/geoip --BandwidthRate 1024000 --BandwidthBurst 1024000 --ControlPort 9051";
 DEFAULT_TORCTL_ARGS = "localhost 9051 STREAM,CIRC,CIRC_MINOR,ORCONN,BW,STREAM_BW,CIRC_BW,CONN_BW,BUILDTIMEOUT_SET,CLIENTS_SEEN,GUARD,CELL_STATS,TB_EMPTY";
 
+default_topology_path = "~/.shadow/share/topology.graphml.xml"
+
+geocodes_pool = [
+             "SD", "NI", "LK", "VG", "NC", "OM", "ME", "MC", "AO", "HT", "LT", "TR",
+             "UY", "GA", "LU", "ML", "MY", "BA", "KZ", "MD", "SG", "JP", "US", "MX",
+             "CN", "CL", "RU", "VI", "ES", "NL", "TN", "GM", "ZW", "DK", "TW", "PK",
+             "CR", "BG", "EC", "AU", "SA", "PG", "HN", "HR", "BN", "BS", "UZ", "JO",
+             "GI", "AI", "QA", "BZ", "TZ", "KH", "AL", "IR", "RO", "CZ", "PT", "GR",
+             "NZ", "NO", "CO", "TH", "AR", "RW", "CD", "KG", "AD", "LA", "GU", "PY",
+             "MA", "KY", "SR", "SN", "BW", "MW", "CY", "NG", "LV", "UA", "FI", "HU",
+             "MO", "DM", "HK", "SK", "JM", "GD", "MU", "AF", "SV", "GL", "BJ", "KW",
+             "YE", "FO", "BF", "PL", "FR", "KR", "AT", "DE", "CA", "CH", "BE", "VN",
+             "IN", "IS", "MN", "MK", "MM", "KE", "SI", "GE", "GG", "CV", "NP", "CI",
+             "NA", "BR", "GB", "IE", "ZA", "UG", "EG", "IT", "SE", "TJ", "IL", "PR",
+             "AE", "BH", "BY", "BT", "GT", "ID", "DO", "PH", "LB", "TT", "MQ", "AG",
+             "MT", "BD", "KN", "RE", "BM", "JE", "BO", "AN", "MR", "LY", "LI", "AM",
+             "TC", "MG", "BB", "FJ", "SZ", "SM", "IM", "AZ", "DZ", "AX", "GP", "MV",
+             "AW", "ZM", "LS", "MZ", "LC", "PE", "PS", "RS", "VE", "EE", "SC", "PA",
+             "GH", "IQ", "SY" ];
+
+random_pool = [];
+
 class ScallionNetwork(object):
 
     root = None;
     killElement = None;
     serverpool = [];
 
-    def __init__(self, entrytag="shadow", killtime = 0):
+    def __init__(self, entrytag="shadow", killtime = 0, random_topology = 0,
+                 no_geohint = 0,
+                 topology_path = default_topology_path):
         self.root = ET.Element(entrytag);
 
         if (killtime != 0):
             self.killElement = ET.SubElement(self.root, "kill");
             self.killElement.set("time", str(killtime));
+
+        self.random_topology = random_topology;
+        self.no_geohint = no_geohint;
+
+        self.topology = ET.SubElement(self.root, "topology");
+        self.setup_topology(path = topology_path); 
 
     def __str__(self):
         rough_string = ET.tostring(self.root, 'utf-8')
@@ -54,42 +84,59 @@ class ScallionNetwork(object):
         plugin.set("id", name);
         plugin.set("path", path + name + ".so");
 
-    def setup_tor(self, node, argv0, start_time=1, tor_id="tor", torctl_id="torctl"):
+
+    # Setup the topology for the net
+    # if random is == 0 then the world (default) graph is used
+    def setup_topology(self, path=default_topology_path):
+        if (not self.no_geohint):
+            if (not self.random_topology):
+                self.topology.set("path", path);
+                self.geocodes_pool = geocodes_pool;
+            else:
+                self.geocodes_pool = random_pool;
+
+    def setup_tor(self, node, argv0, start_time=1, tor_id="scallion", torctl_id="torctl", v3bandwidthsfile = ""):
         # tor
         tor_app = ET.SubElement(node, "application");
         tor_app.set("plugin", tor_id);
         tor_app.set("time", str(start_time));
-        tor_app.set("arguments", argv0 + ' ' +
-                    DEFAULT_TOR_ARGS + ' ' +
-                    "-f ./" + node.get("id") + ".torrc");
+
+        argv = argv0 + ' ' + DEFAULT_TOR_ARGS + ' ' + "-f ./" + node.get("id") + ".torrc";
+
+        if (v3bandwidthsfile != ""):
+            argv = argv + " --V3BandwidthsFile " + v3bandwidthsfile;
+
+        tor_app.set("arguments", argv);
 
         # torctl
         torctl_app = ET.SubElement(node, "application");
         torctl_app.set("plugin", torctl_id);
         torctl_app.set("time", str(start_time + 1));
-        torctl_app.set("arguments", argv0 + ' ' +
-                       DEFAULT_TORCTL_ARGS + ' ' +
-                       "-f ./" + node.get("id") + ".torrc");
+        torctl_app.set("arguments", DEFAULT_TORCTL_ARGS);
 
     def add_tor_control_node(self, id_str, argv0,
                             torctl_id="torctl",
                             quantity = 1,
                             start_time = 1,
+                            geocodehint = "Random",
                             bandwidthdown=10240,
-                            bandwidthup=10240):
+                            bandwidthup=10240,
+                            v3bandwidthsfile = ""):
         node = ET.SubElement(self.root, "node");
         node.set("id", id_str);
-        node.set("bandiwidthdown", str(bandwidthdown));
-        node.set("bandiwidthup", str(bandwidthup));
+        node.set("bandwidthdown", str(bandwidthdown));
+        node.set("bandwidthup", str(bandwidthup));
 
         if (quantity > 1):
             node.set("quantity", str(quantity));
 
-        self.setup_tor(node, argv0, start_time=start_time);
+        self.set_geocode(node, geocodehint);
+
+        self.setup_tor(node, argv0, start_time=start_time, v3bandwidthsfile = v3bandwidthsfile);
 
 
     def add_tor_4autority(self):
-        self.add_tor_control_node("4autority", "dirauth")
+        self.add_tor_control_node("4autority", "dirauth", v3bandwidthsfile = "./data/${NODEID}/dirauth.v3bw")
 
     def add_tor_relay(self, quantity = 1):
         self.add_tor_control_node("relay", "relay", quantity = quantity)
@@ -97,7 +144,7 @@ class ScallionNetwork(object):
     def add_tor_exit(self, quantity = 1):
         self.add_tor_control_node("exit", "exitrelay", quantity = quantity)
 
-    def add_client(self, id_str, plugin, argv, quantity=1, tor=0,
+    def add_client(self, id_str, plugin, argv, quantity=1, tor=0, geocodehint = "Random",
                             start_time=1000, tracked = 0):
         node = ET.SubElement(self.root, "node");
         node.set("id", id_str);
@@ -110,16 +157,16 @@ class ScallionNetwork(object):
             self.setup_tor(node, "client", start_time=start_time - 100);
 
         if (tracked):
-# TODO here we will implement the tracker.
-#            self.setup_tor_tracker_proxy(node, start_time=start_time-1);
-            pass
+            self.setup_tor_tracker_proxy(node, start_time=start_time-1);
 
         ctl_app = ET.SubElement(node, "application");
         ctl_app.set("plugin", plugin);
         ctl_app.set("time", str(start_time));
         ctl_app.set("arguments", argv);
+        self.set_geocode(node, geocodehint);
 
     def add_server(self, id_str, plugin, argv, quantity=1, tor=0,
+                            geocodehint = "Random",
                             start_time=10, tracked = 0):
         self.serverpool.append("id_str")
         node = ET.SubElement(self.root, "node");
@@ -130,18 +177,32 @@ class ScallionNetwork(object):
             self.setup_tor(node, "client", start_time=start_time - 100);
 
         if (tracked):
-# TODO here we will implement the tracker.
-#            self.setup_tor_tracker_proxy(node, start_time=start_time-1);
-            pass
+            self.setup_tor_tracker_proxy(node, start_time=start_time-1);
 
         ctl_app = ET.SubElement(node, "application");
         ctl_app.set("plugin", plugin);
         ctl_app.set("time", str(start_time));
         ctl_app.set("arguments", argv);
+        self.set_geocode(node, geocodehint);
 
+    def setup_tor_tracker_proxy(self, node, start_time = 500):
+        pass
+
+    def set_geocode(self, node, geocodehint = "Random"):
+        if (not self.no_geohint):
+            if (geocodehint == "Random"):
+                geocodehint = random.choice(self.geocodes_pool);
+
+            if (geocodehint != ""):
+                node.set("geocodehint", geocodehint);
 if __name__ == '__main__':
     from sys import argv;
-    sn = ScallionNetwork(killtime = 18000);
+
+    random_topology = 0;
+    if (len(argv) > 1):
+        random_topology = int(argv[1]);
+
+    sn = ScallionNetwork(killtime = 1800, random_topology = random_topology, no_geohint = 0);
 
     sn.add_plugin("filetransfer");
     sn.add_plugin("scallion");
@@ -153,7 +214,7 @@ if __name__ == '__main__':
     sn.add_tor_exit(quantity = 2);
 
     sn.add_server("fileserver", "filetransfer",
-                  "server 80 ~/.shadow/share", tor = 1, tracked = 1);
+                  "server 80 ~/.shadow/share", tor = 0, tracked = 1);
 
     sn.add_client("fileclient", "filetransfer", "client single "
                   + random.choice(sn.serverpool) +
