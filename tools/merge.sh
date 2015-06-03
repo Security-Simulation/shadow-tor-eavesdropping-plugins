35ae01938be14725b12c779080904816c1038d25
Davide Berardi <berardi.dav@gmail.com>
2015-06-03 15:18:08 +0000

    Merge.sh fix

--- tools/merge.sh
+++ tools/merge.sh
@@ -6,12 +6,14 @@ for i in $(ls) ; do
 		for j in $(ls) ; do 
 			if pushd $j > "/dev/null" ; then
 				traced_info=`cat shadow.config.xml | egrep '<application[[:blank:]]+'|egrep 'plugin="autosys"'`
-				straced=`echo $traced_info | egrep 'arguments="[^"]*server[^"]*"'`
-				ctraced=`echo $traced_info | egrep -v 'arguments="[^"]*server[^"]*"'`
+				straced=`echo "$traced_info" | egrep 'arguments="[^"]*server[^"]*"'|wc -l`
+				ctraced=`echo "$traced_info" | egrep -v 'arguments="[^"]*server[^"]*"'|wc -l`
 
 				real_info=`cat shadow.config.xml | egrep '<application[[:blank:]]+'|egrep 'plugin="simpletcp"'`
-				realservers=`echo $real_info | egrep 'arguments="[[:blank:]]*server[^"]*"'
-				realclients=`echo $real_info | egrep 'arguments="[[:blank:]]*client[^"]*"'
+				realservers=`echo "$real_info" | egrep 'arguments="[[:blank:]]*server[^"]*"'|wc -l`
+				realclients=`echo "$real_info" | egrep 'arguments="[[:blank:]]*client[^"]*"'|wc -l`
+
+				echo "TEST: $realservers $realclients $straced $ctraced"
 
 				merged="$merged"$'\n'"idx:$i,$j;$(tail -n 2 pynalyzer.log);real_clients:$realclients;real_traced_clients:$ctraced;real_servers:$realservers;real_traced_servers:$straced"
 				popd > "/dev/null" 
