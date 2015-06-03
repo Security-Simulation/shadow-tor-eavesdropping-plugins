e38f24fa69ff6b8b80eae21ffe3478ad5b89bbb7
Davide Berardi <berardi.dav@gmail.com>
2015-06-03 14:47:09 +0000

    Merge info

--- tools/merge.sh
+++ tools/merge.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-		
-		merged=""
-        for i in $(ls) ; do 
-            if pushd $i > "/dev/null"; then
-                for j in $(ls) ; do 
-                    if pushd $j > "/dev/null" ; then
-						merged="$merged"$'\n'"idx:$i,$j;$(tail -n 2 pynalyzer.log)"
-                        popd > "/dev/null" 
-                    fi
-                done
-                popd > "/dev/null"
-            fi
-        done 
-		
-		echo "$merged"
+
+merged=""
+for i in $(ls) ; do 
+	if pushd $i > "/dev/null"; then
+		for j in $(ls) ; do 
+			if pushd $j > "/dev/null" ; then
+				traced_info=`cat shadow.config.xml | egrep '<application[[:blank:]]+'|egrep 'plugin="autosys"'`
+				straced=`echo $traced_info | egrep 'arguments="[^"]*server[^"]*"'`
+				ctraced=`echo $traced_info | egrep -v 'arguments="[^"]*server[^"]*"'`
+
+				real_info=`cat shadow.config.xml | egrep '<application[[:blank:]]+'|egrep 'plugin="simpletcp"'`
+				realservers=`echo $real_info | egrep 'arguments="[[:blank:]]*server[^"]*"'
+				realclients=`echo $real_info | egrep 'arguments="[[:blank:]]*client[^"]*"'
+
+				merged="$merged"$'\n'"idx:$i,$j;$(tail -n 2 pynalyzer.log);real_clients:$realclients;real_traced_clients:$ctraced;real_servers:$realservers;real_traced_servers:$straced"
+				popd > "/dev/null" 
+			fi
+		done
+		popd > "/dev/null"
+	fi
+done 
+
+echo "$merged"
